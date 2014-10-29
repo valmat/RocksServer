5a9e95c6aa4e1fbd3ce417f64cf39238179b9a2c
valmat <ufabiz@gmail.com>
2014-10-30 04:56:57 +0600

    Changed class PostData

--- src/evhttp/PostData.h
+++ src/evhttp/PostData.h
@@ -26,71 +26,11 @@ namespace RocksServer {
          */
         PostData() : _len(0), _data(nullptr) {}
 
-        /**
-         *  Copy constructor
-         */
-        PostData(const PostData &that) : _len(that._len), _data(that._data) 
-        {
-            std::cout << "\x1b[1;32m Copy constructor \x1b[0m" << std::endl;
-        }
-
-        /**
-         *  Move constructor
-         */
-        PostData(PostData &&that) : _len(std::move(that._len)), _data(std::move(that._data)) 
-        {
-            that._data = nullptr;
-            std::cout << "\x1b[1;32m Move constructor \x1b[0m" << std::endl;
-        }
-
-        /**
-         *  Move assignment
-         */
-        PostData &operator=(PostData &&that)// = default;
-        {
-            if(this != &that) {
-                
-                //_len  = std::move(that._len);
-                //_data = std::move(that._data);
-
-                _len  = that._len;
-                _data = that._data;
-
-
-                that._data = nullptr;
-                that._len = 0;
-            }
-            std::cout << "\x1b[1;32m Move assignment \x1b[0m" << std::endl;
-            std::cout << "(" << std::endl;
-            std::cout << "\tdata:\t"       << (size_t)_data << std::endl;
-            std::cout << "\tthat._data:\t" << (size_t)that._data << std::endl;
-            std::cout << ")" << std::endl << std::endl;
-            return *this;
-        }
-
-        /**
-         *  Copy assignment
-         */
-        PostData &operator=(const PostData &that)// = default;
-        {
-            if(this != &that) {
-                _len  = that._len;
-                _data = that._data;
-            }
-            std::cout << "\x1b[1;32m Copy assignment \x1b[0m" << std::endl;
-            return *this;
-        }
-
         ~PostData()
         {
-            //if(_data) delete [] _data;
-            // \x1b[1;34;47m" << npos << "\t" << lpos << "\x1b[0m"
-            std::cout << "\x1b[1;34;47m data: " << (size_t)_data << "\x1b[0m"<< std::endl;
             if(_data) {
-                std::cout << "\x1b[1;31;47m" << std::string(_data, _len) << "\x1b[0m"<< std::endl;
                 delete [] _data;
                 _data = nullptr;
-                std::cout << "\x1b[1;31;47m will be deleted" << std::endl << "\x1b[0m"<< std::endl;
             }
         }
 
@@ -110,7 +50,6 @@ namespace RocksServer {
             return _data;
         }
 
-
         /**
          *  Cast to a const char *
          */
@@ -120,7 +59,7 @@ namespace RocksServer {
         }
 
         /**
-         *  Cast to a std::string
+         *  Buffer size
          */
         size_t size() const
         {
@@ -150,9 +89,6 @@ namespace RocksServer {
     private:
         size_t _len;
         const char *_data;
-        //std::shared_ptr<const char> _data;
-
-        //const char sep = '\n';
     };
 
 }
