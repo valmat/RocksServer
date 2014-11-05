1451b1ab7f93520181b8bf1a430e9c2088925543
valmat <ufabiz@gmail.com>
2014-11-05 20:44:49 +0600

    updated extension_example/Makefile

--- extension_example/smpl_plug.cpp
+++ extension_example/smpl_plug.cpp
@@ -1,21 +1,23 @@
-// g++ -std=c++11 -shared -fPIC -o smpl_plug.so smpl_plug.cpp
+/**
+ *  smpl_plug.cpp
+ *
+ *  Simple example RocksServer plugin
+ *
+ *  @author valmat <ufabiz@gmail.com>
+ *  @github https://github.com/valmat/rocksserver
+ */
 
-// g++ -std=c++11 -fPIC -I../deps/rocksdb -c smpl_plug.cpp
-// g++ -std=c++11 -shared smpl_plug.o -o smpl_plug.so
-
- // g++ -std=c++11 -fPIC -I../deps/rocksdb/include -c smpl_plug.cpp
-
-// nm smpl_plug.so
-
-// /usr/include/rocksserver/include
-//#include "../src/api.h"
 #include <rocksserver/api.h>
 #include "RequestPing.h"
 
 
 using namespace RocksServer;
 
+/*
+ * Create plugin
+ */
 //PLUGIN(Extension &extension, const RocksDBWrapper& rdb, const IniConfigs& cfg)
+//PLUGIN(Extension &extension, const RocksDBWrapper& rdb)
 PLUGIN(Extension &extension)
 {
     extension.bind("/ping", new RequestPing());
