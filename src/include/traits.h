5746d0cf69d4cdc17bff60c7a8a1da0fe82af3e8
valmat <ufabiz@gmail.com>
2019-03-27 15:14:35 +0500

    Fixed compatibility with C++17 In С++17 std::string::data() was overloaded so it required explicit definition of method signature in SFINAE

--- src/include/traits.h
+++ src/include/traits.h
@@ -12,7 +12,7 @@ namespace RocksServer {
 namespace traits {
 
     namespace sfinae {
-        typedef struct { char _; } yes;
+        typedef struct { char _;   } yes;
         typedef struct { yes _[2]; } no;
     }
 
@@ -20,24 +20,25 @@ namespace traits {
     template <typename T>
     class may_iterable
     {
-        typedef typename std::decay<T>::type X;
+        using decay_t = typename std::decay<T>::type;
         template <typename R> static sfinae::yes test( decltype(&R::begin), decltype(&R::end) ) ;
-        template <typename R> static sfinae::no test(...);
+        template <typename R> static sfinae::no  test(...);
     public:
         // has value std::true_type or std::false_type
-        constexpr static auto value  =  std::integral_constant<bool, sizeof(test<X>(0,0)) == sizeof(sfinae::yes)>();
+        constexpr static auto value = std::integral_constant<bool, sizeof(test<decay_t>(0,0)) == sizeof(sfinae::yes)>();
     };
     
     // This type trait checks if the `data` and `size` methods is defined in the class T
     template <typename T>
     class may_string
     {
-        typedef typename std::decay<T>::type X;
-        template <typename R> static sfinae::yes test( decltype(&R::data), decltype(&R::size) ) ;
-        template <typename R> static sfinae::no test(...);
+        using decay_t = typename std::decay<T>::type;
+
+        template <typename R> static sfinae::yes test( decltype(static_cast<const char*(R::*)() const>(&R::data)), decltype(&R::size) ) ;
+        template <typename R> static sfinae::no  test(...);
     public:
         // has value std::true_type or std::false_type
-        constexpr static auto value  =  std::integral_constant<bool, sizeof(test<X>(0,0)) == sizeof(sfinae::yes)>();
+        constexpr static auto value = std::integral_constant<bool, sizeof(test<decay_t>(0,0)) == sizeof(sfinae::yes)>();
     };
 
 }}
