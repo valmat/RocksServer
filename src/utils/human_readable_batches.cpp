2199b2296aaa9564a0d957501a215500928e4667
valmat <ufabiz@gmail.com>
2024-02-09 20:02:45 +0200

    🔄 Added support for post-write command execution in human-readable batch utility
    - Introduced a new command-line option (`-c`) to specify a command to run on the output file after it's written
    - Implemented `run_cmd` function to execute the specified command on the output file, replacing `{}` in the command with the file name
    - Managed file operations to ensure the command is run after successfully writing batches to files, including error handling and reporting if command execution fails
    - Adjusted return codes for new error conditions related to command execution failures

--- src/utils/human_readable_batches.cpp
+++ src/utils/human_readable_batches.cpp
@@ -11,7 +11,7 @@
 #include <sstream>
 #include <iomanip>
 #include <unistd.h>  // getopt
-#include <cstdlib>  // std::stoul
+#include <cstdlib>  // std::stoul, std::system
 
 #include "db_wrapper.h"
 
@@ -24,6 +24,7 @@ void print_help(const char *script_name) {
               << "-b \tBatch size (number of rows)\n"
               << "-s \tShift from the beginning (number of rows, default: 0)\n"
               << "-l \tLimit on processing (number of rows, default unlimited)\n"
+              << "-c \tCommand to run on file after writing (in format 'cmd {}' )\n"
               << "-h \tPrint current help and exit\n";
 }
 
@@ -35,17 +36,35 @@ std::string gen_name(const std::string& prefix, const std::string& sufix, size_t
     return ss.str();
 }
 
+bool run_cmd(std::string cmd, const std::string& file_name)
+{
+    if (cmd.empty() || file_name.empty()) {
+        return true;
+    }
+
+    size_t pos = cmd.find("{}");
+    if (pos != std::string::npos) {
+        cmd.replace(pos, 2, file_name);
+    } else {
+        std::cerr << "Error: Command does not contain '{}' placeholder." << std::endl;
+        return false;
+    }
+
+    std::cerr << cmd << std::endl;
+
+    return std::system(cmd.c_str()) == 0;
+}
 
 int main(int argc, char **argv)
 {
     char copt=0;
-    std::string database_dir{}, output_prefix{};
+    std::string database_dir{}, output_prefix{}, cmd{};
     std::string sufix = ".txt";
     size_t batch_num = 50000;
     size_t limit = std::numeric_limits<size_t>::max();
     size_t shift = 0;
 
-    while ( (copt = getopt(argc,argv,"f:t:b:e::l::s::h")) != -1) {
+    while ( (copt = getopt(argc,argv,"f:t:b:e::l::s::c::h")) != -1) {
         switch (copt){
             case 'h':
                 print_help(*argv);
@@ -67,6 +86,9 @@ int main(int argc, char **argv)
             break;
             case 's':
                 shift = std::stoul(optarg);
+            break;
+            case 'c':
+                cmd = optarg;
             break;
             case '?':
                 print_help(*argv);
@@ -93,31 +115,49 @@ int main(int argc, char **argv)
     
     std::fstream file;
     std::string file_name;
+    bool are_lines_written = false;
     for (size_t index = 0; it->Valid() && index < limit; ++index, it->Next()) {
         if(index % batch_num == 0) {
+            file.flush();
+            file.close();
+            if(!run_cmd(cmd, file_name)) {
+                std::cerr << "Run '" << cmd << "' on file '" << file_name << "' failed." << std::endl;
+                return 3;
+            }
+
             file_name = gen_name(output_prefix, sufix, index + shift);
             std::cerr << file_name << std::endl;
-            file.close();
             file.open(file_name, std::ios::out | std::ios::binary);
             if (!file) {
                 std::cerr << "Can't open file \"" << file_name << '"' << std::endl;
-                return 3;
+                return 4;
             }
+            are_lines_written = false;
         }
         file << '[' << it->key().ToStringView() << "]\n" 
             << it->value().ToStringView().size() << "\n"
             << it->value().ToStringView() << std::endl;
 
+        are_lines_written = true;
         if (file.fail()) {
             std::cerr << "Can't write file \"" << file_name << '"' << std::endl;
+            file.flush();
             file.close();
-            return 4;
+            return 5;
         }            
     }
 
+    if (are_lines_written) {
+        file.close();
+        if(!run_cmd(cmd, file_name)) {
+            std::cerr << "Run '" << cmd << "' on file '" << file_name << "' failed." << std::endl;
+            return 6;
+        }
+    }    
+
     if (!it->status().ok()) {
         std::cerr << it->status().ToString() << std::endl;
-        return 5;
+        return 7;
     }
 
     return 0;
