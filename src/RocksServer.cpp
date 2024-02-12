08bd3acc4929b84eea34852f99e066dcbe84bdb3
valmat <ufabiz@gmail.com>
2024-02-12 17:59:27 +0200

    Improved clarity and corrected typos in RocksServer.cpp
    - Enhanced wording for better readability.
    - Fixed grammatical errors and improved consistency in comments.
    - Corrected mismatched variable in error message.
    - Clarified configuration descriptions and corrected typo errors.

--- src/RocksServer.cpp
+++ src/RocksServer.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
     /**
      *  
-     *  Prevent server crash on signal SIGPIPE
+     *  Prevent the server from crashing on signal SIGPIPE
      *  
      */    
     if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
      */
     IniConfigs cfg(argv[1]);
     if(!cfg) {
-        std::cerr<<"Error with open file "<< argv[1] << std::endl;
+        std::cerr<<"Failed to open file "<< argv[1] << std::endl;
         return 3;
     }
     DefaultConfigs dfCfg;
@@ -48,11 +48,11 @@ int main(int argc, char **argv)
      *  This option sets the soft limit on the number of simultaneously opened files
      *  (including sockets, pipe, log files, etc).
      *  You can see your current soft limit by command `ulimit -Sn` and your hard limit by command `ulimit -Hn`
-     *  The value of the soft of limit must not exceed the value of hard limit.
+     *  The value of the soft limit must not exceed the value of the hard limit.
      *  If for some reason the server will take a lot of connections,
      *  the number of which exceeds nofile_limit, then part of the connections will not be processed.
      *  Increase nofile_limit can solve the problem.
-     *  if nofile_limit is 0 leaves as default
+     *  If nofile_limit is 0, it is left as the default.
      *  
      */
     auto nofile_limit = cfg.get("nofile_limit", dfCfg.nofile_limit);
@@ -66,16 +66,16 @@ int main(int argc, char **argv)
      *  
      *  Changing process owner (if required)
      *  
-     *  If RocksServer has launched by unprivileged user this adjustment has no effect.
-     *  If RocksServer has launched by root you can change the process owner.
-     *  Specify uid if you would like to RocksServer changing process owner or keep it value 0 to stay as root.
+     *  If RocksServer is launched by an unprivileged user, this adjustment has no effect.
+     *  If RocksServer is launched by root you can change the process owner.
+     *  Specify a uid if you would like RocksServer to change the process owner, or keep its value as 0 to stay as root.
      *  
      */
     {
         unsigned int owner_uid = cfg.get("owner_uid", 0u);
         unsigned int owner_gid = cfg.get("owner_gid", owner_uid);
         if(!change_owner(owner_uid, owner_gid)) {
-            std::cerr << "Failed to change the owner to " << owner_uid << ':' << owner_uid << "." << std::endl;
+            std::cerr << "Failed to change the owner to " << owner_uid << ':' << owner_gid << "." << std::endl;
             return 5;
         }
     }
@@ -111,7 +111,7 @@ int main(int argc, char **argv)
 
     // Check if logger started
     if (!logger) {
-        std::cerr<<"Can't open log file" << std::endl;
+        std::cerr<<"Failed to open log file" << std::endl;
         return 7;
     }
 
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
      *  
      */
     if (!event_init()) {
-        std::cerr << "Failed to init libevent." << std::endl;
+        std::cerr << "Failed to initialize libevent." << std::endl;
         return 8;
     }
 
@@ -145,13 +145,13 @@ int main(int argc, char **argv)
      *  
      */
     EvServerOptions serverOptions;
-    //Set the value to use for the Content-Type header when none was provided. 
+    // Sets the value to use for the Content-Type header when none is provided. 
     serverOptions.content_type     = cfg.get("content_type", dfCfg.content_type);
-    // Sets the what HTTP methods are supported in requests accepted by this server
+    // Sets which HTTP methods are supported in requests accepted by this server.
     serverOptions.allowed_methods  = EVHTTP_REQ_POST | EVHTTP_REQ_GET;
-    // Limitations for body size (limit in bytes. 0 - unlimited)    
+    // Limitations for body size (in bytes; 0 - unlimited)    
     serverOptions.max_body_size    = cfg.get("max_body_size", dfCfg.max_body_size);
-    // Limitations for headers size (limit in bytes. 0 - unlimited)    
+    // Limitations for headers size (in bytes; 0 - unlimited)    
     serverOptions.max_headers_size = cfg.get("max_headers_size", dfCfg.max_headers_size);
     server.setOptions(serverOptions);
     
@@ -179,33 +179,33 @@ int main(int argc, char **argv)
         .bind< RequestSeekNextRange >("/seeknext-range", rdb);
 
     //
-    // If is data base is backupable
+    // If the database is backupable
     //
     rocksdb::BackupableDBOptions bkOptions( cfg.get("backup_path", dfCfg.backup_path) );
 
     if(cfg.get("isbackupable", dfCfg.isbackupable)) {
-        // If share_table_files == true, backup will assume that table files with
+        // When share_table_files is true, the backup will assume that table files with the
         // same name have the same contents. This enables incremental backups and
         // avoids unnecessary data copies.
-        // If share_table_files == false, each backup will be on its own and will
+        // If share_table_files is false, each backup will be on its own and will
         // not share any data with other backups.
         // default: true
         bkOptions.share_table_files = cfg.get("share_table_files",  bkOptions.share_table_files);
-        // If sync == true, we can guarantee you'll get consistent backup even
-        // on a machine crash/reboot. Backup process is slower with sync enabled.
+        // When sync is true, we can guarantee that you'll get a consistent backup even
+        // in the event of a machine crash/reboot. Backup process is slower with sync enabled.
         // If sync == false, we don't guarantee anything on machine reboot. However,
         // chances are some of the backups are consistent.
         // Default: true
         bkOptions.sync              = cfg.get("backup_sync",        bkOptions.sync);
-        // If true, it will delete whatever backups there are already
+        // If true, it will delete any existing backups.
         // Default: false
         bkOptions.destroy_old_data  = cfg.get("backup_destroy_old", bkOptions.destroy_old_data);
-        // If false, we won't backup log files. This option can be useful for backing
+        // If false, we will not back up log files. This option can be useful for backing
         // up in-memory databases where log file are persisted, but table files are in memory.
         // (See https://github.com/facebook/rocksdb/wiki/How-to-persist-in-memory-RocksDB-database%3F)
         // Default: true
         bkOptions.backup_log_files  = cfg.get("backup_log_files",   bkOptions.backup_log_files);
-        // How many background threads will copy files for CreateNewBackup()
+        // Determines how many background threads will copy files for CreateNewBackup()
         // and RestoreDBFromBackup()
         // Default: 1
         bkOptions.max_background_operations
@@ -232,7 +232,7 @@ int main(int argc, char **argv)
      *  
      */
     if (!server.dispatch()) {
-        std::cerr << "Failed to run message loop." << std::endl;
+        std::cerr << "Failed to run the message loop." << std::endl;
         return 10;
     }
 
